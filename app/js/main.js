55909bb4b949aaac82ea03b4c08596028fdf8c76
kosslanoss <88856799+kosslanoss@users.noreply.github.com>
2022-07-11 10:26:09 +0300

    First Commit

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+    $('.menu__btn').toggleClass('menu__btn--active');
+  });
+
+  $('.menu a').on('click', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+
+  $('.hero__link').on('click', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+
+  
+
+});
+
+
+

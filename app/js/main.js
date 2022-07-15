b9ad244a4270eba9f69c5d3d94a9f6a7bb27e216
kosslanoss <88856799+kosslanoss@users.noreply.github.com>
2022-07-15 13:42:57 +0300

    added cursor and motion

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,24 @@
 $(function(){
+  
+  const cursor = document.querySelector('.cursor');
+  const links = document.querySelectorAll('a');
+
+  document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+  })
+
+  links.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+      cursor.classList.add('large');
+    })
+  })
+
+  links.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+      cursor.classList.remove('large');
+    })
+  })
 
   $('.menu__btn').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active');
@@ -22,6 +42,24 @@ $(function(){
       scrollTop: top
     }, 1500);
   });
+
+  $('.btn-up').on('click', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+
+  const swiper = new Swiper('.swiper', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
 
   
 
